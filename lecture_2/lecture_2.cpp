72e231ae8cda816cdead987bec5279eb5be1a249
EionG001 <eglacken001@student.butte.edu>
2025-09-16 20:48:25 +0000

    1

---
+++ lecture_2/lecture_2.cpp
@@ -0,0 +1,12 @@
+//cpp code
+
+#include <cstdio>
+
+int main() {
+    int x = 1;
+    bool b = true; // Boolean value
+    std::printf("%i", b); // Booleans are numbers
+    std::printf("%i", x);
+    std::printf("hello world");
+    return 0;
+}
