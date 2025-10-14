73fe3d87b94eee0c4f736d53320b3f645399aae5
EionG001 <eglacken001@student.butte.edu>
2025-10-14 20:26:22 +0000

    1

--- practice/random.cpp
+++ practice/random.cpp
@@ -23,10 +23,12 @@ struct cart_point {
 
 void print_cart(cart_point point) {
         printf("%d, %d", point.x, point.y);
-        return;
+        return ;
     };
 
 
-    int main() {
-    
+int main() {
+        cart_point point_a = cart_point {5, 2};
+        print_cart(point_a);
+        return 0;
     }
