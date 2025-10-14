36dd5a28d3a24cae13a29fdb42fea46091e09cce
EionG001 <eglacken001@student.butte.edu>
2025-10-14 20:19:10 +0000

    1

---
+++ practice/random.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+/*int main() {
+    //define numbers
+    int A = 50;
+    int B = 25;
+    int c = 30;
+
+    int n = A + B; //step one
+
+    for(int i=0; i<3; i=i+1) {
+        // n - c until n <= 0
+        // 
+        n=n-C;
+        printf("hello there again i = $d\n", i);
+    }
+}*/
+
+struct cart_point {
+        int x;
+        int y;
+    };
+
+void print_cart(cart_point point) {
+        printf("%d, %d", point.x, point.y);
+        return;
+    };
+
+
+    int main() {
+    
+    }
