641892343583f380e85805b277519aca60b02a48
EionG001 <eglacken001@student.butte.edu>
2025-12-09 21:12:39 +0000

    1

--- 12-09-25/obj.hpp
+++ 12-09-25/obj.hpp
@@ -9,7 +9,7 @@ using std::printf;
 
     this object represents something like a simple calculator taking numbers and evaluating
 
-    - problem: representing numbers and operations, solved.
+    - problem: representing numbers and operations, [solved]
     we need an encoding for the numbers and operations of our calculator, lets use a struct
         our structure houses numbers for eval and characters for operations
         the operations will be represented by the following:
@@ -19,15 +19,22 @@ using std::printf;
         - '5' division
         - 'e' evaluation
     
-    - problem: what are the basic operations of this calculator
+    - problem: what are the basic operations of this calculator [solved]
         binary operators on which two numbers will be given (,b)
         - addition: a + b
         - subtraction: a - b 
         - multiplication: a * b
         - division: a / b
-        - evaluation: 
+        - evaluation: symbolizes number for evaluation, termination operation
 
-    - problem: what structure defines the equations integrity preserving order.
+    - problem: what structure defines the equations integrity preserving order. [solved]
+        stack<dicho> st.
+        - stack of type dicho b/c dicho assist in evaluation operation encoding.
+    
+    - problem: we need to be able to load this stack (st) in proper order. [unsolved]
+
+    - problem: we need a way to evaluate a full stack. [unsolved]
+
     */
 
     class Calc{
@@ -37,6 +44,12 @@ using std::printf;
         char op;
         };
     public:
+    stack<dicho> st;
+    int addition (int , int)
+    int subtraction (int , int)
+    int multiplication (int , int)
+    int division (int , int)
+    int . (int , int)
     };
 
     
